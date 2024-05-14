17271f27d963d0d9064cb1f7e1ed74ed4d23ad42
ambuj-dubey <shriniwasambuj8055@gmail.com>
2024-05-14 15:14:25 +0530

    #52 Creating REsponsive Sidebar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,18 @@
 console.log("this is script file")
+
+
+const togglesidebar= () => {
+
+    const sidebar = $(".sidebar");
+  const content = $(".content");
+
+  if (sidebar.is(":visible")) {
+    // Hide sidebar
+    sidebar.hide();
+    content.css("margin-left", "0%");
+  } else {
+    // Show sidebar
+    sidebar.show();
+    content.css("margin-left", "20%");
+  }
+};
