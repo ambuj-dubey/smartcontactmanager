df56835ab3bf88c50b37ca253ba5b2d0c7b849d4
ambuj-dubey <shriniwasambuj8055@gmail.com>
2024-05-14 21:12:48 +0530

    #55 Uploading mage with contact details

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
@@ -50,14 +58,37 @@ public class UserController {
 	
 	//processing add contact form
 	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact, Principal principal) {
-		String name=principal.getName();
-		User user =this.userRepository.getUserByUserName(name);			//here we fetched the user
-		contact.setUser(user);											//before this line was added I was not gettting the userid in the contact tabele
-		user.getContacts().add(contact);								//then contact is added to the user
-		this.userRepository.save(user);									//then save it
-		System.out.println("Added to database");
-		System.out.println("DATA "+contact);
+	public String processContact(
+			@ModelAttribute Contact contact,
+			@RequestParam("profileImage") MultipartFile file,
+			Principal principal) {
+		try {
+						
+			String name=principal.getName();
+			User user =this.userRepository.getUserByUserName(name);			//here we fetched the user
+			
+			//processing and uploading file
+			if(file.isEmpty()) {
+				//if file is eampty then try our message
+				
+			}else {
+				//upload file to folder and update to contact
+				contact.setImage(file.getOriginalFilename());
+				File saveFile = new ClassPathResource("static/img").getFile();
+				Path path=Paths.get(saveFile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+				Files.copy(file.getInputStream(),path , StandardCopyOption.REPLACE_EXISTING);
+				System.out.println("Image is uploaded");
+			}
+			
+			contact.setUser(user);											//before this line was added I was not gettting the userid in the contact tabele
+			user.getContacts().add(contact);								//then contact is added to the user
+			this.userRepository.save(user);									//then save it
+			System.out.println("Added to database");
+			System.out.println("DATA "+contact);
+		}catch(Exception e) {
+			System.out.println("ERROR "+ e.getMessage());
+			e.printStackTrace();
+		}
 		return "normal/add_contact_form";
 	}
 }
