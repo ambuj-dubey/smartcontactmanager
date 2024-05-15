bc739fce104fd996793f9b1c73403e7e477334b7
ambuj-dubey <shriniwasambuj8055@gmail.com>
2024-05-15 22:54:22 +0530

    #59 Showing all contacts in effectie manner

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.aspectj.bridge.Message;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -32,6 +34,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 	//method to hiding common data to response
 	@ModelAttribute
 	public void addCommonData(Model model, Principal principal) {
@@ -101,4 +106,25 @@ public class UserController {
 		}
 		return "normal/add_contact_form";
 	}
+	//show contacts handler
+	@GetMapping("/show-contacts")
+	public String showContacts(Model m, Principal principal) {
+		m.addAttribute("title", "Show Contacts");
+		//contacts ki list bhejana hai
+		/*this was used usinf the Principal method
+		 * String userName = principal.getName(); User user =
+		 * this.userRepository.getUserByUserName(userName); user.getContacts();
+		 * List<Contact> contacts = user.getContacts();
+		 */
+		String userName = principal.getName();
+		User user = this.userRepository.getUserByUserName(userName);
+		List<Contact> contacts = this.contactRepository.findContactsByUser(user.getId());
+		m.addAttribute("contacts",contacts);
+		return "normal/show_contacts";
+	}
+	
+	
+	
+	
+	
 }
