877b50d03c93b43d52b54ea51b3d12dff13e5571
ambuj-dubey <shriniwasambuj8055@gmail.com>
2024-05-20 11:02:58 +0530

    #70 Cretaed search contact

--- src/main/java/com/smart/dao/ContactRepository.java
+++ src/main/java/com/smart/dao/ContactRepository.java
@@ -1,5 +1,7 @@
 package com.smart.dao;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.smart.entities.Contact;
+import com.smart.entities.User;
 
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 	
@@ -14,4 +17,7 @@ public interface ContactRepository extends JpaRepository<Contact, Integer> {
 	//current page
 	//contact per page -5
 	public Page<Contact> findContactsByUser(@Param("userId") int userID, Pageable pePageable);
+	
+	//search ... This is created by JPA
+	public List<Contact> findByNameContainingAndUser(String name,User user); //here findBy is used to create the find method and 'name' is the value which will be passed from the frontend and then Containing is a keyword which is used to check then next attribute like here User
 }
